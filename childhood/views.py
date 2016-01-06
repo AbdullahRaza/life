298d31158562cd3846d191c1869c9952c387b18a
AbdullahRaza <abdullah.raza17@gmail.com>
2016-01-06 17:31:56 +0500

    Working app

--- childhood/views.py
+++ childhood/views.py
@@ -6,4 +6,8 @@ __author__ = 'Abdullah'
 
 def home(request):
     var = 5
-    return render_to_response('home.html',{'var':var})
+    return render_to_response('home.html')
+
+
+def dummy(request):
+    return render_to_response('dummy.html')
